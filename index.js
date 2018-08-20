13321afdd7eaf808987cefa9793d5a8c049758d2
Helder Pereira <helcorintho@gmail.com>
2018-08-20 10:57:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var express = require('express')
-var app = expres()
+var app = express()
 
 app.set('port', (process.env.PORT || 5000))
-app.use(expres.static(__dirname + '/public'))
+app.use(express.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Fiap 2TINA')
