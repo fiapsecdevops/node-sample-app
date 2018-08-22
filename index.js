a7275ad155547673c6e1da0196974a88a3491935
Helder Pereira <helcorintho@gmail.com>
2018-08-21 21:47:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express')
 var app = express()
 
 app.set('port', (process.env.PORT || 1337))
-app.use(express.static(__dirname + '/public'))
+app.use(expres.static(__dirname + '/public'))
 
 app.get('/', function(request, response) {
   response.send('Fiap 2018 TESTE Deploy')
